2e6ecb16d036ed93d2450942f98b5eb116b1c6b6
tingkai <tingkai@csie.io>
2019-04-15 22:36:01 +0800

    leetcode 119

--- (0119) pascal's triangle ii.cpp
+++ (0119) pascal's triangle ii.cpp
@@ -1,35 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-	void traversal_tree(TreeNode *node, map<int, int>& level_map, int current_level) {
-		if (node == NULL) {
-			return;
-		}
-		
-		if (level_map.find(current_level) == level_map.end()) {
-			level_map.insert(pair<int, int>(current_level, node->val));
-		}
-		
-		traversal_tree(node->right, level_map, current_level+1);
-		traversal_tree(node->left, level_map, current_level+1);
-	}
-	
-	vector<int> rightSideView(TreeNode* root) {
-		vector<int> result;
-		map<int, int> level_max;
-		traversal_tree(root, level_max, 0);
-		for (map<int, int>::iterator it = level_max.begin(); it != level_max.end(); it++) {
-			result.push_back(it->second);
-		}
-		return result;
-	}
+    vector<int> getRow(int rowIndex) {
+        /*vector<int> result;
+        int half_idx = (rowIndex % 2 == 0) ? (rowIndex/2+1) : (rowIndex/2);
+        for (int i = 0; i < rowIndex+1; i++) {
+            if (i == 0) {
+                result.push_back(1);
+            } else if (i == 1) {
+                result.push_back(rowIndex);
+            } else if (i > half_idx) {
+                result.push_back(result[rowIndex-i]);
+            } else {
+                unsigned long long int value = result[i-1] * (rowIndex-i+1) / 2;
+                result.push_back((int)value);
+            }
+        }
+        return result;*/
+        vector<vector<int>> result = {{1}};
+        if (rowIndex == 0) {
+            return result[0];
+        }
+        for (int i = 1; i < rowIndex+1; i++) {
+            vector<int> pre_row = result[i-1];
+            vector<int> row = {1};
+            for (int j = 1; j < i; j++) {
+                row.push_back(pre_row[j-1] + pre_row[j]);
+            }
+            row.push_back(1);
+            result.push_back(row);
+        }
+        return result[rowIndex];
+    }
 };
 
