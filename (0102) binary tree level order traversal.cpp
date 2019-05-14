39184dfa5175df96b35969110d024c831d985a72
tingkai <tingkai@csie.io>
2019-05-14 20:22:35 +0800

    faster way

--- (0102) binary tree level order traversal.cpp
+++ (0102) binary tree level order traversal.cpp
@@ -8,34 +8,33 @@
  * };
  */
 class Solution {
+	vector<vector<int>> result = {};
 public:
-	void traversal(vector<vector<int>>& result, TreeNode* root, int level) {
-		if (result.size()-1 >= level) {
-			result[level].push_back(root->val);
-		} else {
-			vector<int> list = { root->val };
-			result.push_back(list);
-		}
-		
-		if (root->left != NULL) {
-			traversal(result, root->left, level+1);
-		}
-		
-		if (root->right != NULL) {
-			traversal(result, root->right, level+1);
-		}
-		
-		if (root->left == NULL && root->right == NULL) {
-			return;
-		}
-	}
-	
 	vector<vector<int>> levelOrder(TreeNode* root) {
-		vector<vector<int>> result = {{}};
 		if (root == NULL) {
-			return {};
+			return result;
 		}
-		traversal(result, root, 0);
+		queue<TreeNode*> q;
+		q.push(root);
+		
+		while (!q.empty()) {
+			int qsize = q.size();
+			vector<int> nodes = {};
+			while (qsize--) {
+				TreeNode *node = q.front(); 
+				nodes.push_back(node->val);
+				q.pop();
+				if (node->left) {
+					q.push(node->left);
+				}
+				if (node->right) {
+					q.push(node->right);
+				}
+			}
+			result.push_back(nodes);
+		}
+		
 		return result;
 	}
 };
+
